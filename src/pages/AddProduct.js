c23ddf26a1be322adb8184dbe193f056804bf877
Vovus)xd <101352111+Vovusus@users.noreply.github.com>
2023-12-08 18:28:37 +0300

    add product

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from'axios'
 
 function AddProduct() {
-    return (
-        <div className='addProduct'>
-            <input className='productImgLink' type='text'/>
-            <input className='productName' type='text'/>
-            <input className='productDesc' type='text'/>
-            <input className='productPrice' type='text'/>
-            <input className='productCategory' tupe='text'/>
-            <botton>Добавить продукт</botton>
-        </div>
-    );
-}
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-function onClick() {
-    
+    const onSubmit = async(data) => {
+        const product = {
+            "title": data.productTitle,
+            "price": data.productPrice,
+            "description": data.productDescription,
+            "image": data.productImage,
+            "category": data.productCategory
+        }
+        console.log(product)
+        await axios (
+            axios.post("https://fakestoreapi.com/products", product).then(response => alert(response))
+        )
+    }
+
+    return (
+        <form className='addProduct' onSubmit={handleSubmit(onSubmit)}>
+            <div className='add_container'>
+                <input className='productImgLink' type='text' placeholder='Ссылка на картинку' {...register("productImage", {required: true})}/>
+                <input className='productName' type='text' placeholder='Название' {...register("productTitle", {required: true})}/>
+                <input className='productDesc' type='text' placeholder='Описание' {...register("productDescription", {required: true})}/>
+                <input className='productPrice' type='text' placeholder='Стоимость' {...register("productPrice", {required: true})}/>
+                <input className='productCategory' tupe='text' placeholder='Категория' {...register("productCategory", {required: true})}/>
+                <button type='submit'>Добавить продукт</button>
+            </div>
+        </form>
+    );
 }
 
 export default AddProduct;
